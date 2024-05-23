561d85072ba3e13202c77bb449a15adc45e73b36
Akshay <akshay@bpract.com>
2024-05-23 20:53:48 +0530

    added the get task ids function and updated the entire add and delete functionality

--- todo_smart_contract/contracts/TodoList.sol
+++ todo_smart_contract/contracts/TodoList.sol
@@ -1,8 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.5.16;
 
+// Todo: When redusing count the data gets invalidated
 contract TodoList {
     uint public taskCount = 0;
+    uint[] public availableTaskIds;
+    mapping(uint => Task) public tasks;
 
     struct Task {
         uint id;
@@ -10,25 +13,29 @@ contract TodoList {
         bool completed;
     }
 
-    mapping(uint => Task) public tasks;
+    event Toggling(uint id, string content, bool completed);
+    event Toggled(uint id, string content, bool completed);
 
     constructor() public {
         addTask("Deploy the Smart Contract");
     }
 
-    event Toggling(uint id, string content, bool completed);
-    event Toggled(uint id, string content, bool completed);
-
     function addTask(string memory _content) public returns (bool) {
         taskCount++;
         tasks[taskCount] = Task(taskCount, _content, false);
+        availableTaskIds.push(taskCount);
         return true;
     }
 
     function removeTask(uint _id) public returns (bool) {
         if (taskExists(_id)) {
             delete tasks[_id];
-            taskCount--;
+            for (uint i = 0; i < availableTaskIds.length; i++) {
+                if (availableTaskIds[i] == _id) {
+                    availableTaskIds[i] = 0;
+                    break;
+                }
+            }
             return true;
         }
         return false;
@@ -50,5 +57,37 @@ contract TodoList {
         require(_id > 0 && task.id == _id, "Task Does not exist");
 
         return true;
+    }
+
+    function getTaskIds() public view returns (uint[] memory) {
+        uint availableTaskLength = 0;
+        for (uint i = 0; i < availableTaskIds.length; i++) {
+            if (availableTaskIds[i] > 0) {
+                availableTaskLength++;
+            }
+        }
+
+        uint[] memory ids = new uint[](availableTaskLength);
+        uint index = 0;
+        for (uint i = 0; i < availableTaskIds.length; i++) {
+            if (availableTaskIds[i] > 0) {
+                ids[index] = availableTaskIds[i];
+                index++;
+            }
+        }
+
+        return ids;
+
+        // uint counter = 0;
+        // uint i = 1;
+        // Task memory task;
+        // while (counter < taskCount) {
+        //     task = tasks[i];
+        //     if (task.id == i) {
+        //         ids[counter] = task.id;
+        //         counter++;
+        //     }
+        //     i++;
+        // }
     }
 }
